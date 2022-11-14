91161575cca1bc5166c0be761e7035d8d69acfd1
saulgordillo <108088343+saulgordillo@users.noreply.github.com>
2022-11-14 12:52:14 +0100

    Add files via upload

---
+++ project1/footer.php
@@ -0,0 +1,11 @@
+<?php ?>
+
+</div>
+</main>
+
+<footer class="footer">
+    Copyright Lucas Jiménez & Saül Gordillo; 2022
+</footer>
+
+</body>
+</html>
---
+++ project1/header.php
@@ -0,0 +1,30 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="styling.css" />
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar">
+    <div class="logo" href="index.html">
+        <a href="index.html">
+            <img src="Pokeball-PNG-Image-Transparent-Background.png" class ="logo1" alt="logo" href="index.html">
+        </a>
+        <div class="logo2"><a href="index.html"> Pokeshop</a> </div>
+    </div>
+    <ul class="nav-links">
+        <div class="menu">
+            <li><a href="catalog.php">Comprar</a></li>
+            <li><a href="about.php">Sobre nosotros</a></li>
+            <li><a href="login.php">Login</a></li>
+        </div>
+    </ul>
+</nav>
+
+
+<main>
+<div class ="container">
