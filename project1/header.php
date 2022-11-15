0e891f7e8a1b78fcff3fd124f89870f527a43071
LucasJimenezN <ljimenez2nunez@gmail.com>
2022-11-15 09:25:52 +0100

    Update

--- project1/header.php
+++ project1/header.php
@@ -1,4 +1,4 @@
-<?php include "../config/database.php"
+<?php include "config/database.php"
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -6,14 +6,14 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/styling.css" />
+    <link rel="stylesheet" href="css/styling.css" />
     <title>PokeShop</title>
 </head>
 <body>
 <nav class="navbar">
     <div class="logo" href="index.php">
         <a href="index.php">
-            <img src="../img/Pokeball-PNG-Image-Transparent-Background.png" class ="logo1" alt="logo" href="index.php">
+            <img src="img/Pokeball-PNG-Image-Transparent-Background.png" class ="logo1" alt="logo" href="index.php">
         </a>
         <div class="logo2"><a href="index.php"> Pokeshop</a> </div>
     </div>
